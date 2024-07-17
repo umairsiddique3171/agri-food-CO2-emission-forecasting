7a70e2cfae9f99ff0aefec62591a254791a5809f
Umair Siddique <umairsiddique3171@gmail.com>
2024-07-17 18:29:25 +0500

    updated exception.py and logger.py

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,30 @@
 import sys
+from logger import *
 
-def error_message(error,error_detail:sys):
-    pass
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info() 
+    filename = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+    error_message="Error occured in python script : name[{0}] ; line number[{1}] ; error message[{2}]".format(
+        filename,
+        line_no,
+        str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+# testing 
+try : 
+    a = 1/0
+except Exception as e:
+    logging.info(CustomException(e,sys).error_message)
+    raise CustomException(e,sys)
